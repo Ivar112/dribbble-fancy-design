79fa51d578e3df2f742db7bf4ecf4842541e9185
Ivar Polman <ivar@gek.nl>
2022-12-07 23:29:58 +0100

    chore: full size img on full page

--- src/app.js
+++ src/app.js
@@ -77,43 +77,52 @@ document.addEventListener('keydown', function(e) {
         document.querySelectorAll('.main-col-right').forEach(element => {
             element.classList.add('move-left');
             setTimeout(function () {element.classList.remove('move-left')}, 1000);
-         });
-         document.querySelectorAll('.slide').forEach(element => {    
-             if (element.classList.contains('slide-1')) {
-     
-                 setTimeout(function () {removeClassByPrefix(element, 'slide-')}, 1000);
-                 setTimeout(function () {element.classList.add('slide-' + slideCount)}, 1000);
-             } else {
-                 for (let i = 1; i <= slideCount; i++) {
-                     if(element.classList.contains('slide-' + i)) {
-                         setTimeout(function () {removeClassByPrefix(element, 'slide-')}, 1000);
-                         setTimeout(function () {element.classList.add('slide-' + (i - 1))}, 1000);
-                     }
-                 }
-             }
-         });
-      
-         document.body.classList.add('transitioning');
-         setTimeout(function () {document.body.classList.remove('transitioning')}, 1000);
-         setTimeout(function () { 
-         if (i == slideCount) {
-             i = 1
-         } else {
-             i++;
-         }
-         pageCount.innerHTML = '/0' + i;
-         }, 375);
-         
-         counter.classList.add('animating');
-         setTimeout(function () {removeClassByPrefix(counter, 'count-')}, 1000);
-         if (j == slideCount) {
-             j = 1
-         } else {
-             j++;
-         }
-         setTimeout(function () {counter.classList.add('count-' + j)}, 1000);
-         setTimeout(function () {counter.classList.remove('animating')}, 1000);
-
+        });
+        document.querySelectorAll('.slide').forEach(element => {    
+            if (element.classList.contains('slide-1')) {
+    
+                setTimeout(function () {removeClassByPrefix(element, 'slide-')}, 1000);
+                setTimeout(function () {element.classList.add('slide-' + slideCount)}, 1000);
+            } else {
+                for (let i = 1; i <= slideCount; i++) {
+                    if(element.classList.contains('slide-' + i)) {
+                        setTimeout(function () {removeClassByPrefix(element, 'slide-')}, 1000);
+                        setTimeout(function () {element.classList.add('slide-' + (i - 1))}, 1000);
+                    }
+                }
+            }
+        });
+    
+        document.body.classList.add('transitioning');
+        setTimeout(function () {document.body.classList.remove('transitioning')}, 1000);
+        setTimeout(function () { 
+        if (i == slideCount) {
+            i = 1
+        } else {
+            i++;
+        }
+        pageCount.innerHTML = '/0' + i;
+        }, 375);
+        
+        counter.classList.add('animating');
+        setTimeout(function () {removeClassByPrefix(counter, 'count-')}, 1000);
+        if (j == slideCount) {
+            j = 1
+        } else {
+            j++;
+        }
+        setTimeout(function () {counter.classList.add('count-' + j)}, 1000);
+        setTimeout(function () {counter.classList.remove('animating')}, 1000);
     }
  
+});
+
+document.getElementById('fullPage1').addEventListener('click', function(e) {
+    document.querySelectorAll('.full-page-1')[0].classList.add("show");
+    document.body.classList.add('page-open');
+});
+
+document.querySelectorAll('.btn-close').forEach(element => {
+        element.addEventListener('click', function (e) {element.parentElement.classList.remove('show')
+    });
 });
